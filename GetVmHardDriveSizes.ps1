cf2125bb3cb1d28aa9eb545ea1046561618f400b
RasClarque <51036676+RasClarque@users.noreply.github.com>
2019-05-26 14:45:50 -0700

    Add files via upload

---
+++ GetVmHardDriveSizes.ps1
@@ -0,0 +1,13 @@
+﻿clear
+$VMs = Get-VM
+Foreach ($VM in $VMs)
+{
+  $HardDrives = $VM.HardDrives
+  Foreach ($HardDrive in $HardDrives)
+  {
+    $hddsize = $HardDrive.path | Get-VHD | select path,size
+    $hddsize.Size = $hddsize.Size / 1GB
+    $hddsize | format-table Path,size
+    
+  }
+}
